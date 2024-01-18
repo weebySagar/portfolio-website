0ff4495f519882201f411151d57bf09eb3fe8b3e
Sagar Moriya <sagarmaurya814@gmail.com>
2024-01-18 12:39:14 +0530

    integrated db and cloudinary

--- strapi-backend/config/plugins.js
+++ strapi-backend/config/plugins.js
@@ -4,4 +4,18 @@ module.exports = ({ env }) => ({
         defaultDepth: 5, // Default is 5
       }
     },
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
   });
