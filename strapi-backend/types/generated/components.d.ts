1414549364b20b7b8a4c0fb7e179b3e781064427
Sagar Moriya <sagarmaurya814@gmail.com>
2024-01-18 11:40:21 +0530

    added header component

--- strapi-backend/types/generated/components.d.ts
+++ strapi-backend/types/generated/components.d.ts
@@ -1,5 +1,34 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface NavigationbrandlogoNavigationBrandLogo
+  extends Schema.Component {
+  collectionName: 'components_navigationbrandlogo_navigation_brand_logos';
+  info: {
+    displayName: 'Navigation Brand Logo';
+  };
+  attributes: {
+    Link: Attribute.String & Attribute.Required & Attribute.DefaultTo<'/'>;
+    BrandLogo: Attribute.Media & Attribute.Required;
+  };
+}
+
+export interface NavigationmenuNavigationMenu extends Schema.Component {
+  collectionName: 'components_navigationmenu_navigation_menus';
+  info: {
+    displayName: 'Navigation Menu';
+    description: '';
+  };
+  attributes: {
+    Link: Attribute.String & Attribute.Required;
+    Logo: Attribute.Media & Attribute.Required;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'navigationbrandlogo.navigation-brand-logo': NavigationbrandlogoNavigationBrandLogo;
+      'navigationmenu.navigation-menu': NavigationmenuNavigationMenu;
+    }
+  }
 }
--- strapi-backend/types/generated/contentTypes.d.ts
+++ strapi-backend/types/generated/contentTypes.d.ts
@@ -362,24 +362,19 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
-export interface ApiHeaderHeader extends Schema.CollectionType {
+export interface ApiHeaderHeader extends Schema.SingleType {
   collectionName: 'headers';
   info: {
     singularName: 'header';
     pluralName: 'headers';
     displayName: 'Header';
-    description: '';
   };
   options: {
     draftAndPublish: true;
   };
   attributes: {
-    Link: Attribute.String & Attribute.Required & Attribute.DefaultTo<'/'>;
-    Icon: Attribute.Media & Attribute.Required;
-    BrandLogoURL: Attribute.String &
-      Attribute.Required &
-      Attribute.DefaultTo<'/'>;
-    BrandLogoImage: Attribute.Media & Attribute.Required;
+    NavigationMenu: Attribute.Component<'navigationmenu.navigation-menu', true>;
+    NavigationBrand: Attribute.Component<'navigationbrandlogo.navigation-brand-logo'>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
@@ -609,6 +604,50 @@ export interface PluginContentReleasesReleaseAction
   };
 }
 
+export interface PluginI18NLocale extends Schema.CollectionType {
+  collectionName: 'i18n_locale';
+  info: {
+    singularName: 'locale';
+    pluralName: 'locales';
+    collectionName: 'locales';
+    displayName: 'Locale';
+    description: '';
+  };
+  options: {
+    draftAndPublish: false;
+  };
+  pluginOptions: {
+    'content-manager': {
+      visible: false;
+    };
+    'content-type-builder': {
+      visible: false;
+    };
+  };
+  attributes: {
+    name: Attribute.String &
+      Attribute.SetMinMax<{
+        min: 1;
+        max: 50;
+      }>;
+    code: Attribute.String & Attribute.Unique;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'plugin::i18n.locale',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'plugin::i18n.locale',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUsersPermissionsPermission
   extends Schema.CollectionType {
   collectionName: 'up_permissions';
@@ -760,50 +799,6 @@ export interface PluginUsersPermissionsUser extends Schema.CollectionType {
   };
 }
 
-export interface PluginI18NLocale extends Schema.CollectionType {
-  collectionName: 'i18n_locale';
-  info: {
-    singularName: 'locale';
-    pluralName: 'locales';
-    collectionName: 'locales';
-    displayName: 'Locale';
-    description: '';
-  };
-  options: {
-    draftAndPublish: false;
-  };
-  pluginOptions: {
-    'content-manager': {
-      visible: false;
-    };
-    'content-type-builder': {
-      visible: false;
-    };
-  };
-  attributes: {
-    name: Attribute.String &
-      Attribute.SetMinMax<{
-        min: 1;
-        max: 50;
-      }>;
-    code: Attribute.String & Attribute.Unique;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'plugin::i18n.locale',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'plugin::i18n.locale',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
 declare module '@strapi/types' {
   export module Shared {
     export interface ContentTypes {
@@ -819,10 +814,10 @@ declare module '@strapi/types' {
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
+      'plugin::i18n.locale': PluginI18NLocale;
       'plugin::users-permissions.permission': PluginUsersPermissionsPermission;
       'plugin::users-permissions.role': PluginUsersPermissionsRole;
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
-      'plugin::i18n.locale': PluginI18NLocale;
     }
   }
 }
