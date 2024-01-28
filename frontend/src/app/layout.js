92be2e04d3a13494cb87a0a88772161a02ec0826
Sagar Moriya <sagarmaurya814@gmail.com>
2024-01-28 17:29:47 +0530

    integrated page transitions

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -24,7 +24,9 @@ crossOrigin="anonymous" />
         <Header/>
         {/* <SmoothScrolling> */}
 
-        {children}
+       <main>
+         {children}
+        </main>
         {/* </SmoothScrolling> */}
         <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></Script>
 
