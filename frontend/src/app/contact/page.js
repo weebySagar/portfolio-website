b628ea1ca2a17899d2e5f83f94e743064240e25a
Sagar Moriya <sagarmaurya814@gmail.com>
2024-01-30 22:15:43 +0530

    integrated contact us page

---
+++ frontend/src/app/contact/page.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import "@/styles/about/_about.scss";
+import Window from '@/components/window/Window';
+
+const ContactPage = () => {
+
+  return (
+
+    <section className='about vh-100'>
+
+      <div className="container d-flex align-items-center h-100">
+        <Window />
+
+      </div>
+    </section>
+  )
+}
+
+export default ContactPage
