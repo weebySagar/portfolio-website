286243aecb1e7e12043a34b5a2f388da182bc453
Sagar Moriya <sagarmaurya814@gmail.com>
2024-02-04 18:26:40 +0530

    added scroll listener on social links

--- frontend/src/components/header/NavigationSocialLinks.jsx
+++ frontend/src/components/header/NavigationSocialLinks.jsx
@@ -1,9 +1,27 @@
+'use client'
+
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const NavigationSocialLinks = ({ socialLinks }) => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      setIsScrolled(scrollPosition > 200);
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener('scroll', handleScroll);
+
+    // Remove the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
-    <div className="navbar-social-links">
+    <div className={`navbar-social-links ${isScrolled && "d-none"}`}>
       {socialLinks?.map((social) => (
         <Link href={social?.Link} target="_blank" key={social?.id} title={social?.SocialName}>
           <div className={social?.SocialName}></div>
